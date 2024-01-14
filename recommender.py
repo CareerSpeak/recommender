f9a2d7044e79243d12de0ad173fcf108b4a1e347
Tanmay Kulkarni <91152390+Darshula@users.noreply.github.com>
2024-01-14 13:48:21 +0000

    Initial upload

---
+++ recommender.py
@@ -0,0 +1,61 @@
+import argparse
+import json
+import re
+
+import PyPDF2
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+
+with open('data.json') as json_data:
+    job_role_dataset = json.load(json_data)
+
+
+# Function to extract text from a PDF file
+def extract_text_from_pdf(pdf_path):
+    with open(pdf_path, 'rb') as file:
+        pdf_reader = PyPDF2.PdfReader(file)
+        text = ''
+        for page_num in range(len(pdf_reader.pages)):
+            page = pdf_reader.pages[page_num]
+            text += page.extract_text()
+    return text
+
+
+# Function to clean and preprocess text data
+def preprocess_text(text):
+    # Remove non-alphanumeric characters and convert to lowercase
+    text = re.sub(r'[^a-zA-Z0-9\-\'\@\+\.\/\s]', '', text)
+    text = text.lower()
+    # print(text)
+    return text
+
+
+# Function to recommend job roles based on keywords
+def recommend_job_role(keywords):
+    vectorizer = TfidfVectorizer()
+    keyword_matrix = vectorizer.fit_transform([' '.join(job_role_dataset[job])
+                                               for job in job_role_dataset])
+
+    user_keywords = ' '.join(keywords)
+    user_vector = vectorizer.transform([user_keywords])
+
+    similarities = cosine_similarity(user_vector, keyword_matrix).flatten()
+    recommended_job_index = similarities.argmax()
+    recommended_job_role = list(job_role_dataset.keys())[recommended_job_index]
+
+    return recommended_job_role
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('filepath', type=str,
+                        help='Path to resume pdf file')
+    args = parser.parse_args()
+
+    pdf_path = args.filepath
+    resume_text = extract_text_from_pdf(pdf_path)
+    preprocessed_resume_text = preprocess_text(resume_text)
+    keywords = preprocessed_resume_text.split()
+    recommended_job_role = recommend_job_role(keywords)
+
+    print(f'Recommended Job Role: {recommended_job_role}')
