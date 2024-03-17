27bd13dea0a0b5f2c57bf9d34751c2f3b7c11f5c
Darshula <tanmayvijaykulkarni2002@gmail.com>
2024-03-18 00:27:22 +0530

    removing pdf functions and ability to run without flask

--- recommender.py
+++ recommender.py
@@ -1,21 +1,8 @@
-import argparse
 import json
 import re
 
-import PyPDF2
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
-
-
-# Function to extract text from a PDF file
-def extract_text_from_pdf(pdf_path):
-    with open(pdf_path, 'rb') as file:
-        pdf_reader = PyPDF2.PdfReader(file)
-        text = ''
-        for page_num in range(len(pdf_reader.pages)):
-            page = pdf_reader.pages[page_num]
-            text += page.extract_text()
-    return text
 
 
 # Function to clean and preprocess text data
@@ -42,18 +29,3 @@ def recommend_job_role(keywords):
     recommended_job_role = list(job_role_dataset.keys())[recommended_job_index]
 
     return recommended_job_role
-
-
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
-    parser.add_argument('filepath', type=str,
-                        help='Path to resume pdf file')
-    args = parser.parse_args()
-
-    pdf_path = args.filepath
-    resume_text = extract_text_from_pdf(pdf_path)
-    preprocessed_resume_text = extract_keywords(resume_text)
-    keywords = preprocessed_resume_text.split()
-    recommended_job_role = recommend_job_role(keywords)
-
-    print(f'Recommended Job Role: {recommended_job_role}')
